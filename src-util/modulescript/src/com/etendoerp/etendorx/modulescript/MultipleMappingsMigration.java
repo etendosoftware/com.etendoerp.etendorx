911e9d0b128f90fdff2274643a1c0227d93df8de
sebastianbarrozo <sebastian.barrozo@etendo.software>
2023-12-14 14:04:15 -0300

    Feature EPL-974: Requested changes

--- src-util/modulescript/src/com/etendoerp/etendorx/modulescript/MultipleMappingsMigration.java
+++ src-util/modulescript/src/com/etendoerp/etendorx/modulescript/MultipleMappingsMigration.java
@@ -18,33 +18,32 @@ import org.openbravo.utils.FormatUtilities;
 import org.openbravo.database.ConnectionProvider;
 
 public class MultipleMappingsMigration extends ModuleScript {
-  private String ad_module_id = "BC7B2F721FD249F5A360C6AAD2A7EBF7";
+  private final String AD_MODULE_ID = "BC7B2F721FD249F5A360C6AAD2A7EBF7";
 
   @Override
   protected ExecutionLimits getExecutionLimits() {
-    return new ModuleScriptExecutionLimits(ad_module_id, null,
-        new OpenbravoVersion(1,5,0));
+    return new ModuleScriptExecutionLimits(AD_MODULE_ID, null, new OpenbravoVersion(1, 5, 0));
   }
 
   @Override
   public void execute() {
     try {
       ConnectionProvider cp = getConnectionProvider();
-      DatabaseMetaData metaData = cp.getConnection().getMetaData();
-      PreparedStatement ps = cp
-          .getPreparedStatement("UPDATE etrx_projection_entity e " +
-              "SET    NAME = ( Upper(p.NAME) || ' - ' || t.NAME || ' - ' || " +
-              "CASE e.mapping_type WHEN 'R' THEN 'Read' ELSE 'Write' END ) " +
-              "FROM   etrx_projection p, ad_table t " +
-              "WHERE  p.etrx_projection_id = e.etrx_projection_id " +
-              "AND t.ad_table_id = e.ad_table_id AND e.name IS NULL");
+      PreparedStatement ps = cp.getPreparedStatement(
+          new StringBuilder().append("UPDATE etrx_projection_entity e ")
+              .append("SET    NAME = ( Upper(p.NAME) || ' - ' || t.NAME || ' - ' || ")
+              .append("CASE e.mapping_type WHEN 'R' THEN 'Read' ELSE 'Write' END ) ")
+              .append("FROM   etrx_projection p, ad_table t ")
+              .append("WHERE  p.etrx_projection_id = e.etrx_projection_id ")
+              .append("AND t.ad_table_id = e.ad_table_id AND e.name IS NULL")
+              .toString());
       ps.executeUpdate();
-      ps = cp
-          .getPreparedStatement("UPDATE etrx_projection_entity e " +
-              "SET external_name = t.name " +
-              "FROM etrx_projection p, ad_table t " +
-              "WHERE p.etrx_projection_id = e.etrx_projection_id " +
-              "AND t.ad_table_id = e.ad_table_id AND e.external_name IS NULL");
+      ps = cp.getPreparedStatement(new StringBuilder().append("UPDATE etrx_projection_entity e ")
+          .append("SET external_name = t.name ")
+          .append("FROM etrx_projection p, ad_table t ")
+          .append("WHERE p.etrx_projection_id = e.etrx_projection_id ")
+          .append("AND t.ad_table_id = e.ad_table_id AND e.external_name IS NULL")
+          .toString());
       ps.executeUpdate();
     } catch (Exception e) {
       handleError(e);
