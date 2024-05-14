e3d9f883aa75f466851bfc3a53b78003b1abdcfe
Roman Magnoli <roman.magnoli@etendo.software>
2024-05-14 16:31:11 -0300

    EPL-1495: Make buildConfig available to all services

--- src/com/etendoerp/etendorx/BuildConfig.java
+++ src/com/etendoerp/etendorx/BuildConfig.java
@@ -25,19 +25,36 @@ import java.net.URL;
 import com.etendoerp.etendorx.data.ETRXConfig;
 import com.etendoerp.etendorx.data.ETRXoAuthProvider;
 
-public class BuildConfig extends HttpBaseServlet {
+/**
+ * This class is the base class for the build configuration servlets. It provides the basic
+ * functionality for retrieving the default configuration and updating it with the OAuth providers
+ * details.
+ */
+public abstract class BuildConfig extends HttpBaseServlet {
 
   private static final Logger log = LogManager.getLogger();
   public static final String SPRING_SECURITY_OAUTH_2_CLIENT_REGISTRATION = "spring.security.oauth2.client.registration.";
   public static final String SPRING_SECURITY_OAUTH_2_CLIENT_PROVIDER = "spring.security.oauth2.client.provider.";
 
+  /**
+   * This method handles the GET request. It fetches the default configuration, updates it with the OAuth providers details and sends the response.
+   * It also handles the creation of a new source if no default configuration is found for the specific service.
+   *
+   * @param request The HttpServletRequest object.
+   * @param response The HttpServletResponse object.
+   */
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) {
     try {
-      OBContext.setOBContext("0");
-      JSONObject result = getDefaultConfigToJsonObject();
-      SimpleEntry<Integer, JSONObject> sourceEntry = findSource(result);
-      updateSourceWithOAuthProviders(sourceEntry.getValue());
+      OBContext.setAdminMode();
+      final String serviceURI = getURIFromRequest(request);
+      final String service = serviceURI.split("/")[1];
+      final JSONObject result = getDefaultConfigToJsonObject(serviceURI);
+      SimpleEntry<Integer, JSONObject> sourceEntry = findSource(result.getJSONArray("propertySources"), service);
+      // TODO: Improve the way to check if the service needs the oAuthProvider configuration.
+      if (StringUtils.equals("auth", service) || StringUtils.equals("psd2", service)) {
+        updateSourceWithOAuthProviders(sourceEntry.getValue());
+      }
       sendResponse(response, result, sourceEntry.getValue(), sourceEntry.getKey());
     } catch (Exception e) {
       log.error(e.getMessage(), e);
@@ -48,16 +65,27 @@ public class BuildConfig extends HttpBaseServlet {
   }
 
   /**
+   * This method extracts the URI from the given HTTP request.
+   *
+   * @param request The HTTP request from which the URI is to be extracted.
+   * @return The URI as a string. It is the part of the request URL that comes after the servlet path.
+   */
+  private static String getURIFromRequest(HttpServletRequest request) {
+    // TODO: Improve the way to get the URI
+    return request.getRequestURL().toString().split(request.getServletPath())[1];
+  }
+
+  /**
    * This method gets the default configuration from the ETRXConfig entity and converts it to a JSON object.
    *
    * @return The default configuration as a JSON object.
    * @throws JSONException If there is an error parsing the JSON.
    */
-  private JSONObject getDefaultConfigToJsonObject() throws JSONException, IOException {
+  private JSONObject getDefaultConfigToJsonObject(String serviceURI) throws JSONException, IOException {
     ETRXConfig rxConfig = (ETRXConfig) OBDal.getInstance().createCriteria(ETRXConfig.class)
         .setMaxResults(1)
         .uniqueResult();
-    URL url = new URL(rxConfig.getConfigURL() + "/auth/default");
+    URL url = new URL(rxConfig.getConfigURL() + serviceURI);
     HttpURLConnection conn = (HttpURLConnection) url.openConnection();
     conn.setRequestMethod("GET");
     final InputStream inputStream = conn.getInputStream();
@@ -72,21 +100,24 @@ public class BuildConfig extends HttpBaseServlet {
   }
 
   /**
-   * This method finds the source with the server port 8094 from the result JSON object.
+   * This method finds the source JSON object in the property sources array.
+   * If no default config is found for the specific service, it retrieves the application default config.
    *
-   * @param result The result JSON object.
-   * @return The source JSON object.
+   * @param propSource The property sources array.
+   * @param service The service name.
+   * @return The index and the source JSON object.
    * @throws JSONException If there is an error parsing the JSON.
    */
-  private SimpleEntry<Integer, JSONObject> findSource(JSONObject result) throws JSONException {
-    JSONArray propSource = result.getJSONArray("propertySources");
-    for (int i = 0; i < propSource.length(); i++) {
-      JSONObject source = propSource.getJSONObject(i).getJSONObject("source");
-      if (StringUtils.equals(source.getString("server.port"), "8094")) {
-        return new SimpleEntry<>(i, source);
+  private SimpleEntry<Integer, JSONObject> findSource(JSONArray propSource, String service) throws JSONException {
+    final int length = propSource.length();
+    for (int i = 0; i < length; i++) {
+      if (propSource.getJSONObject(i).getString("name").contains(service)) {
+        return new SimpleEntry<>(i, propSource.getJSONObject(i).getJSONObject("source"));
       }
     }
-    return new SimpleEntry<>(-1, new JSONObject());
+    // If no default config is found for the specific service, retrieve the application default config.
+    // That it's always load at the last index of the JSONArray
+    return new SimpleEntry<>(length - 1, propSource.getJSONObject(length - 1).getJSONObject("source"));
   }
 
   /**
@@ -125,12 +156,23 @@ public class BuildConfig extends HttpBaseServlet {
       sourceJSON.put(SPRING_SECURITY_OAUTH_2_CLIENT_PROVIDER + providerName + ".token-uri", apiUrl + provider.getTokenuri());
       sourceJSON.put(SPRING_SECURITY_OAUTH_2_CLIENT_PROVIDER + providerName + ".user-info-uri",  apiUrl + provider.getUserinfouri());
       sourceJSON.put(SPRING_SECURITY_OAUTH_2_CLIENT_PROVIDER + providerName + ".user-name-attribute", provider.getUsernameattribute());
+      // Here the abstract method is called to fill custom configurations.
+      fillCustomConfigs(sourceJSON, provider);
     } catch (JSONException e) {
       log.error(e.getMessage(), e);
       throw new OBException(e);
     }
   }
 
+  /**
+   * This method sends the response to the client.
+   *
+   * @param response The HTTP response.
+   * @param result The result JSON object.
+   * @param sourceJSON The source JSON object.
+   * @param indexFound The index of the source JSON object in the property sources array.
+   * @throws IOException If there is an error writing the response.
+   */
   private void sendResponse(HttpServletResponse response, JSONObject result, JSONObject sourceJSON, Integer indexFound) throws IOException {
     response.setContentType("application/json");
     response.setCharacterEncoding("utf-8");
@@ -146,4 +188,19 @@ public class BuildConfig extends HttpBaseServlet {
   public void doPost(HttpServletRequest request, HttpServletResponse response) {
     throw new OBException("POST not supported");
   }
+
+  /**
+   * This abstract method is used to fill custom configurations in the source JSON object.
+   * Implementing classes should add configurations that follow the same structure as a YAML file.
+   * <p>
+   * For example, to add a client-id for a specific OAuth provider, the following code can be used:
+   * <code>sourceJSON.put("spring.security.oauth2.client.registration." + providerName +
+   * ".client-id", provider.getIdforclient());</code>
+   * <p>
+   * This method should be implemented by any class that extends BuildConfig.
+   *
+   * @param sourceJSON The source JSON object where the custom configurations should be added.
+   * @param provider The OAuth provider for which the custom configurations are being added.
+   */
+  public abstract void fillCustomConfigs(JSONObject sourceJSON, ETRXoAuthProvider provider);
 }
