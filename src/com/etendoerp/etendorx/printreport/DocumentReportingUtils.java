614c2b853dd1d8998f1c7b86774cc266c300f4a5
sebastianbarrozo <sebastian.barrozo@etendo.software>
2025-12-22 12:10:18 -0300

    Feature ETP-3012: Docs print servlet

---
+++ src/com/etendoerp/etendorx/printreport/DocumentReportingUtils.java
@@ -0,0 +1,223 @@
+package com.etendoerp.etendorx.printreport;
+
+import java.io.ByteArrayOutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.openbravo.base.ConfigParameters;
+import org.openbravo.base.exception.OBException;
+import org.openbravo.base.structure.BaseOBObject;
+import org.openbravo.client.kernel.KernelServlet;
+import org.openbravo.dal.core.OBContext;
+import org.openbravo.dal.service.OBDal;
+import org.openbravo.model.ad.ui.Tab;
+import org.openbravo.model.ad.ui.Process;
+import org.openbravo.client.application.report.ReportingUtils;
+import org.openbravo.client.application.report.ReportingUtils.ExportType;
+import org.openbravo.erpCommon.utility.reporting.ReportManager;
+import org.openbravo.erpCommon.utility.reporting.Report;
+import org.openbravo.erpCommon.utility.reporting.DocumentType;
+import org.openbravo.base.secureApp.VariablesSecureApp;
+import org.openbravo.client.kernel.RequestContext;
+import org.openbravo.database.ConnectionProvider;
+import org.openbravo.model.common.order.Order;
+import org.openbravo.service.db.DalConnectionProvider;
+
+/**
+ * Utility class for generating document reports in PDF format.
+ * It handles standard reports, Jasper reports defined in the dictionary, and custom Java processes.
+ */
+public class DocumentReportingUtils {
+
+  /**
+   * Private constructor to prevent instantiation.
+   */
+  private DocumentReportingUtils() {
+  }
+
+  /**
+   * Generates the PDF for a record given its tabId and recordId.
+   * Considers customized printing processes defined on the tab as well as the standard flow.
+   *
+   * @param tabId
+   *     The ID of the tab where the record is located.
+   * @param recordId
+   *     The ID of the record to generate the PDF for.
+   * @return A byte array containing the PDF data.
+   * @throws Exception
+   *     If an error occurs during PDF generation.
+   */
+  public static byte[] generatePDF(String tabId, String recordId) {
+    byte[] pdfData = null;
+    try {
+      OBContext.setAdminMode();
+      Tab tab = OBDal.getInstance().get(Tab.class, tabId);
+      if (tab == null) {
+        throw new OBException("Tab not found with ID: " + tabId);
+      }
+
+      pdfData = tryCustomProcess(tab, recordId);
+
+      if (pdfData == null) {
+        DocumentType docType = determineDocumentType(tab, recordId);
+        if (docType != DocumentType.UNKNOWN) {
+          pdfData = generateStandardReport(docType, recordId);
+        }
+      }
+
+      if (pdfData == null) {
+        throw new OBException("No print method found for table: " + tab.getTable().getDBTableName());
+      }
+    } catch (Exception e) {
+      throw new OBException(e);
+    } finally {
+      OBContext.restorePreviousMode();
+    }
+    return pdfData;
+  }
+
+  /**
+   * Attempts to generate a PDF using a custom process defined on the tab.
+   *
+   * @param tab
+   *     The tab definition.
+   * @param recordId
+   *     The ID of the record.
+   * @return A byte array containing the PDF data, or null if no custom process is defined.
+   * @throws Exception
+   *     If an error occurs during custom process execution.
+   */
+  private static byte[] tryCustomProcess(Tab tab, String recordId) throws Exception {
+    byte[] result = null;
+    if (tab.getProcess() != null) {
+      Process process = tab.getProcess();
+      if (Boolean.TRUE.equals(process.isJasperReport())) {
+        result = generateJasperProcess(process, recordId);
+      } else if (process.getJavaClassName() != null) {
+        result = executeCustomJavaProcess(process.getJavaClassName(), recordId);
+      }
+    }
+    return result;
+  }
+
+  /**
+   * Determines the DocumentType for a given tab and record.
+   *
+   * @param tab
+   *     The tab definition.
+   * @param recordId
+   *     The ID of the record.
+   * @return The determined DocumentType, or DocumentType.UNKNOWN if not found.
+   */
+  private static DocumentType determineDocumentType(Tab tab, String recordId) {
+    DocumentType docType = DocumentType.UNKNOWN;
+    BaseOBObject o = OBDal.getInstance().get(tab.getTable().getName(), recordId);
+    String tableName = tab.getTable().getDBTableName();
+
+    if ("C_ORDER".equalsIgnoreCase(tableName) && o instanceof Order) {
+      Order order = (Order) o;
+      docType = Boolean.TRUE.equals(order.isSalesTransaction()) ? DocumentType.SALESORDER : DocumentType.PURCHASEORDER;
+    } else if ("C_INVOICE".equalsIgnoreCase(tableName)) {
+      docType = DocumentType.SALESINVOICE;
+    } else if ("M_INOUT".equalsIgnoreCase(tableName)) {
+      docType = DocumentType.SHIPMENT;
+    } else if ("FIN_PAYMENT".equalsIgnoreCase(tableName)) {
+      docType = DocumentType.PAYMENT;
+    }
+
+    return docType;
+  }
+
+  /**
+   * Generates a PDF using a Jasper report process defined in the dictionary.
+   *
+   * @param process
+   *     The process definition containing the Jasper template information.
+   * @param recordId
+   *     The ID of the record to be used as a parameter for the report.
+   * @return A byte array containing the PDF data.
+   */
+  private static byte[] generateJasperProcess(Process process, String recordId) {
+    // The JRNAME usually contains the path to the .jrxml (eg: @basedesign@/org/...)
+    String jrxmlPath = process.getJRTemplateName();
+    Map<String, Object> parameters = new HashMap<>();
+    // By convention the parameter is usually DOCUMENT_ID or the PK column name
+    parameters.put("DOCUMENT_ID", recordId);
+
+    ByteArrayOutputStream os = new ByteArrayOutputStream();
+    ReportingUtils.exportJR(jrxmlPath, ExportType.PDF, parameters, os, false, new DalConnectionProvider(false), null, null);
+    return os.toByteArray();
+  }
+
+  /**
+   * Executes a custom Java process to generate a report.
+   * Note: Standard scheduler processes do not return binary data directly.
+   *
+   * @param className
+   *     The fully qualified name of the Java class to execute.
+   * @param recordId
+   *     The ID of the record to be processed.
+   * @return A byte array containing the PDF data (currently returns empty bytes).
+   * @throws Exception
+   *     If an error occurs during process execution.
+   */
+  private static byte[] executeCustomJavaProcess(String className, String recordId) throws Exception {
+    Class<?> clazz = Class.forName(className);
+    if (org.openbravo.scheduling.Process.class.isAssignableFrom(clazz)) {
+      org.openbravo.scheduling.Process process = (org.openbravo.scheduling.Process) clazz.getDeclaredConstructor().newInstance();
+
+      VariablesSecureApp vars = RequestContext.get().getVariablesSecureApp();
+      ConnectionProvider cp = new DalConnectionProvider(false);
+
+      // Create and initialize the bundle
+      // We don't have a Process definition ID here; passing null for the processId.
+      // If necessary the Process entity could be looked up when 'className' maps to a Process.
+      org.openbravo.scheduling.ProcessBundle bundle = new org.openbravo.scheduling.ProcessBundle(null, vars).init(cp);
+
+      // Set parameters expected by standard processes
+      Map<String, Object> params = new HashMap<>();
+      params.put("recordId", recordId); // Typical parameter name; sometimes it's the primary key column name.
+      bundle.setParams(params);
+
+      // Execute the process
+      process.execute(bundle);
+
+      // Standard scheduler processes do not return binary data directly.
+      // If the custom process produces a PDF it usually stores it as an attachment or in params.
+      // Return empty bytes because extracting a PDF from an arbitrary Process is not generally possible.
+      return new byte[0];
+    }
+    throw new UnsupportedOperationException("Custom class " + className + " must implement 'org.openbravo.scheduling.Process'");
+  }
+
+  /**
+   * Generates a standard report for a given document type.
+   *
+   * @param docType
+   *     The type of document (e.g., Sales Order, Invoice).
+   * @param recordId
+   *     The ID of the record to generate the report for.
+   * @return A byte array containing the PDF data.
+   * @throws Exception
+   *     If an error occurs during report generation.
+   */
+  private static byte[] generateStandardReport(DocumentType docType, String recordId) {
+    try {
+      ConnectionProvider cp = new DalConnectionProvider(false);
+      VariablesSecureApp vars = RequestContext.get().getVariablesSecureApp();
+
+      final ConfigParameters servletConfiguration = KernelServlet.getGlobalParameters();
+      final ReportManager reportManager = new ReportManager(servletConfiguration.strFTPDirectory,
+          null, servletConfiguration.strBaseDesignPath, servletConfiguration.strDefaultDesignPath,
+          servletConfiguration.prefix, false);
+      Report report = new Report(cp, docType, recordId, vars.getLanguage(), "default", false,
+          Report.OutputTypeEnum.PRINT);
+
+      // The ReportManager fills the JasperPrint
+      net.sf.jasperreports.engine.JasperPrint jp = reportManager.processReport(report, vars);
+      return net.sf.jasperreports.engine.JasperExportManager.exportReportToPdf(jp);
+    } catch (Exception e) {
+      throw new OBException("Error generating standard report for document type: " + docType, e);
+    }
+  }
+}
---
+++ src/com/etendoerp/etendorx/services/DocumentPrintServlet.java
@@ -0,0 +1,219 @@
+package com.etendoerp.etendorx.services;
+
+import java.io.OutputStream;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+import org.openbravo.base.exception.OBException;
+import org.openbravo.client.kernel.KernelServlet;
+import org.openbravo.client.kernel.RequestContext;
+import org.openbravo.erpCommon.utility.reporting.DocumentType;
+import org.openbravo.service.web.WebService;
+
+import com.etendoerp.etendorx.printreport.DocumentReportingUtils;
+
+/**
+ * Web service servlet that handles document printing requests.
+ * It receives parameters like tabId, recordId, and docType to generate and return a PDF document.
+ */
+public class DocumentPrintServlet implements WebService {
+  private static final Logger log4j = LogManager.getLogger(DocumentPrintServlet.class);
+
+  /**
+   * Handles GET requests to generate and stream a PDF document.
+   *
+   * @param path
+   *     The request path.
+   * @param request
+   *     The HTTP servlet request containing 'tabId', 'recordId', and 'docType' parameters.
+   * @param response
+   *     The HTTP servlet response where the PDF stream will be written.
+   * @throws Exception
+   *     If an error occurs during PDF generation or streaming.
+   */
+  @Override
+  public void doGet(String path, HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    String tabId = request.getParameter("tabId");
+    String recordId = request.getParameter("recordId");
+    DocumentType docType = parseDocumentType(request.getParameter("docType"));
+
+    if (isInvalidRequest(tabId, recordId, docType)) {
+      sendBadRequest(response);
+      return;
+    }
+
+    setupRequestContext(request);
+
+    try {
+      byte[] pdfBytes = DocumentReportingUtils.generatePDF(tabId, recordId);
+      sendPdfResponse(response, pdfBytes);
+    } catch (Exception e) {
+      handleError(response, e);
+    }
+  }
+
+  /**
+   * Parses the document type from a string parameter.
+   *
+   * @param docTypeParam
+   *     The document type string.
+   * @return The corresponding DocumentType enum, or DocumentType.UNKNOWN if parsing fails.
+   */
+  private DocumentType parseDocumentType(String docTypeParam) {
+    if (docTypeParam == null) {
+      return DocumentType.UNKNOWN;
+    }
+    try {
+      return DocumentType.valueOf(docTypeParam.toUpperCase());
+    } catch (IllegalArgumentException e) {
+      return DocumentType.UNKNOWN;
+    }
+  }
+
+  /**
+   * Validates if the request parameters are valid.
+   *
+   * @param tabId
+   *     The tab ID.
+   * @param recordId
+   *     The record ID.
+   * @param docType
+   *     The document type.
+   * @return true if any required parameter is missing or invalid.
+   */
+  private boolean isInvalidRequest(String tabId, String recordId, DocumentType docType) {
+    return tabId == null || recordId == null || docType == DocumentType.UNKNOWN;
+  }
+
+  /**
+   * Sends a 400 Bad Request response with a descriptive message.
+   *
+   * @param response
+   *     The HTTP servlet response.
+   * @throws Exception
+   *     If an error occurs while writing the response.
+   */
+  private void sendBadRequest(HttpServletResponse response) {
+    response.setStatus(HttpServletResponse.SC_BAD_REQUEST);
+    try {
+      response.getWriter().write("Parameters 'tabId', 'docType' and 'recordId' are required.");
+    } catch (Exception e) {
+      log4j.error("Error sending bad request response", e);
+    }
+  }
+
+  /**
+   * Ensures the RequestContext is populated with the current request.
+   *
+   * @param request
+   *     The HTTP servlet request.
+   */
+  private void setupRequestContext(HttpServletRequest request) {
+    if (RequestContext.get().getRequest() == null) {
+      RequestContext.get().setRequest(request);
+    }
+  }
+
+  /**
+   * Streams the PDF bytes to the HTTP response.
+   *
+   * @param response
+   *     The HTTP servlet response.
+   * @param pdfBytes
+   *     The byte array containing the PDF data.
+   * @throws Exception
+   *     If an error occurs while writing to the output stream.
+   */
+  private void sendPdfResponse(HttpServletResponse response, byte[] pdfBytes) {
+    response.setContentType("application/pdf");
+    response.setHeader("Content-Disposition", "inline; filename=document.pdf");
+    response.setContentLength(pdfBytes.length);
+    try (OutputStream os = response.getOutputStream()) {
+      os.write(pdfBytes);
+      os.flush();
+    } catch (Exception e) {
+      throw new OBException(e.getMessage(), e);
+    }
+  }
+
+  /**
+   * Handles errors by logging them and sending a 500 Internal Server Error response.
+   *
+   * @param response
+   *     The HTTP servlet response.
+   * @param e
+   *     The exception that occurred.
+   * @throws Exception
+   *     If an error occurs while writing the error response.
+   */
+  private void handleError(HttpServletResponse response, Exception e) {
+    try {
+      log4j.error("Error generating PDF document", e);
+      response.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+      e.printStackTrace(response.getWriter());
+    } catch (Exception ex) {
+      log4j.error("Error sending error response", ex);
+    }
+  }
+
+  /**
+   * Handles POST requests. Currently not implemented.
+   *
+   * @param path
+   *     The request path.
+   * @param request
+   *     The HTTP servlet request.
+   * @param response
+   *     The HTTP servlet response.
+   * @throws Exception
+   *     If an error occurs.
+   */
+  @Override
+  public void doPost(String path, HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    // Not implemented
+
+  }
+
+  /**
+   * Handles DELETE requests. Currently not implemented.
+   *
+   * @param path
+   *     The request path.
+   * @param request
+   *     The HTTP servlet request.
+   * @param response
+   *     The HTTP servlet response.
+   * @throws Exception
+   *     If an error occurs.
+   */
+  @Override
+  public void doDelete(String path, HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    // Not implemented
+
+  }
+
+  /**
+   * Handles PUT requests. Currently not implemented.
+   *
+   * @param path
+   *     The request path.
+   * @param request
+   *     The HTTP servlet request.
+   * @param response
+   *     The HTTP servlet response.
+   * @throws Exception
+   *     If an error occurs.
+   */
+  @Override
+  public void doPut(String path, HttpServletRequest request, HttpServletResponse response)
+      throws Exception {
+    // Not implemented
+
+  }
+}
