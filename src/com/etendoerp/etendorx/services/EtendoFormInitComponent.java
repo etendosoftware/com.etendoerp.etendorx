f975b713bb106146db7088c7518da5aa2aa23c9e
Francisco Roig <francisco.roig@smfconsulting.es>
2026-01-23 15:45:23 -0300

    Issue #163: Fix StringUtils import
    ETP-3197

--- src/com/etendoerp/etendorx/services/EtendoFormInitComponent.java
+++ src/com/etendoerp/etendorx/services/EtendoFormInitComponent.java
@@ -16,7 +16,7 @@ package com.etendoerp.etendorx.services;
  *************************************************************************
  */
 
-import org.apache.commons.lang.StringUtils;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONObject;
