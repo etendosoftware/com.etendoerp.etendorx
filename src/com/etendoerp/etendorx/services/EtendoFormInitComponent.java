e107f2b6844d17b2fa5a06db703a1b062d6f6ebd
Francisco Roig <francisco.roig@smfconsulting.es>
2026-01-23 15:26:15 -0300

    Issue #163: Add Etendo license and stringUtils checks
    ETP-3197

--- src/com/etendoerp/etendorx/services/EtendoFormInitComponent.java
+++ src/com/etendoerp/etendorx/services/EtendoFormInitComponent.java
@@ -1,5 +1,22 @@
 package com.etendoerp.etendorx.services;
+/*
+ *************************************************************************
+ * The contents of this file are subject to the Etendo License
+ * (the "License"), you may not use this file except in compliance with
+ * the License.
+ * You may obtain a copy of the License at
+ * https://github.com/etendosoftware/etendo_core/blob/main/legal/Etendo_license.txt
+ * Software distributed under the License is distributed on an
+ * "AS IS" basis, WITHOUT WARRANTY OF ANY KIND, either express or
+ * implied. See the License for the specific language governing rights
+ * and limitations under the License.
+ * All portions are Copyright © 2021–2026 FUTIT SERVICES, S.L
+ * All Rights Reserved.
+ * Contributor(s): Futit Services S.L.
+ *************************************************************************
+ */
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.codehaus.jettison.json.JSONObject;
@@ -75,7 +92,7 @@ public class EtendoFormInitComponent extends org.openbravo.client.application.wi
   private void initializeTabMetadata(Map<String, Object> parameters) {
     try {
       String tabId = (String) parameters.get(TAB_ID);
-      if (tabId == null || "null".equals(tabId)) {
+      if (tabId == null || StringUtils.equals(tabId, "null")) {
         log.debug("No tab ID provided, skipping metadata initialization");
         return;
       }
